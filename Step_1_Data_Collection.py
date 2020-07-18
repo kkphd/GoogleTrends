ee04e88852dc49546e3597b02dd2aa9c7bbba711
kkphd <57160239+kkphd@users.noreply.github.com>
2020-07-18 17:18:50 -0400

    Updated data collection code.

--- Step_1_Data_Collection.py
+++ Step_1_Data_Collection.py
@@ -15,55 +15,63 @@ class Trend:
         self.end_date = str(end_date)
         self.time_period = start_date + ' ' + end_date
         self.df1 = None
-        self.df2 = None
-        self.df = None
         self.cols1 = None
+        self.df1_metro = None
+        self.df2 = None
         self.cols2 = None
-        self.country = None
+        self.df2_metro = None
         self.agreement = None
+        self.df = None
+        self.df_geo = None
         self.symptoms1_df()
         self.symptoms2_df()
+        self.merge_df()
+        self.adjust_df()
         analysis_date = datetime.now()
         print('Data was collected on ' + analysis_date.strftime("%B %d, %Y") + ' at ' +
-              analysis_date.strftime("%H:%M:%S"))
+              analysis_date.strftime("%H:%M:%S") + ' for searches between ' + start_date +
+              ' and ' + end_date + '.')
 
     def symptoms1_df(self):
         kw_list1 = ['concussion', 'mild traumatic brain injury', 'mTBI', 'post-concussion syndrome', 'ding']
         pytrend.build_payload(kw_list1, timeframe=self.time_period)
         self.df1 = pytrend.interest_over_time()
         self.cols1 = pd.DatetimeIndex.to_frame(self.df1.axes[0])
-        self.df1.insert(loc=0, column='Date1', value=self.cols1)
+        self.df1.insert(loc=0, column='Date', value=self.cols1)
+        self.df1_metro = pytrend.interest_by_region(resolution='DMA', inc_geo_code=True)
 
     def symptoms2_df(self):
         kw_list2 = ['dinged', 'bell-ringer', 'bell rung', 'football', 'NFL']
         pytrend.build_payload(kw_list2, timeframe=self.time_period)
         self.df2 = pytrend.interest_over_time()
         self.cols2 = pd.DatetimeIndex.to_frame(self.df2.axes[0])
-        self.df2.insert(loc=0, column='Date2', value=self.cols2)
+        self.df2.insert(loc=0, column='Date', value=self.cols2)
+        self.df2_metro = pytrend.interest_by_region(resolution='DMA', inc_geo_code=True)
 
     def merge_df(self):
-        self.agreement = pd.concat([self.df1.Date1, self.df2.Date2], axis=1)
-        self.agreement['Agree'] = np.where(self.agreement.Date1 == self.agreement.Date2, 1, 0)
+        self.agreement = pd.concat([self.df1.Date, self.df2.Date], axis=1)
+        self.agreement['Agree'] = np.where(self.agreement.iloc[:, 0] ==
+                                           self.agreement.iloc[:, 1], 1, 0)
 
         if self.agreement.shape[0] == self.agreement['Agree'].count():
-            self.df = pd.merge(left=self.df1, right=self.df2, how='inner')
+            self.df = pd.merge(left=self.df1, right=self.df2, how='left')
+            self.df.set_index(self.df.iloc[:, 0])
         else:
             print("DataFrames' indices do not agree - double check values.")
 
-
     def adjust_df(self):
-        df = pd.DataFrame.drop('isPartial')
-        df = pd.DataFrame.append()
+        self.df.drop(['isPartial'], axis=1, inplace=True)
+        self.df.set_index('Date')
 
         # Combine columns of similar content or rename variables.
-        df['mTBI'] = df['mild traumatic brain injury'] + df['mTBI']
-        df['pcs'] = df['post-concussion syndrome']
-        df['ding'] = df['ding'] + df['dinged']
-        df['bell'] = df['bell-ringer'] + df['bell rung']
+        self.df['mTBI'] = self.df['mild traumatic brain injury'] + self.df['mTBI']
+        self.df['pcs'] = self.df['post-concussion syndrome']
+        self.df['ding'] = self.df['ding'] + self.df['dinged']
+        self.df['bell'] = self.df['bell-ringer'] + self.df['bell rung']
 
 
 
 # Example
-start_date = "2015-07-01"
+start_date = "2010-07-01"
 end_date = "2020-07-01"
 t = Trend(start_date, end_date)
